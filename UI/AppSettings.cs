d33b3d0b1b18de9c372c67805ef7ecec5d860055
Valyon450 <valentinbasuk@gmail.com>
2024-06-25 13:34:12 +0300

    feat(UI): Add AppSettings and implementation of Dependency Injection.

---
+++ UI/AppSettings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.Storage;
+using Windows.UI.Xaml;
+
+namespace UI
+{
+    public class AppSettings
+    {
+        private ElementTheme _appTheme;
+        private const string LanguageKey = "AppLanguage";
+
+        private readonly ApplicationDataContainer localSettings;
+
+        public AppSettings()
+        {
+            localSettings = ApplicationData.Current.LocalSettings;
+        }
+
+        public ElementTheme AppTheme
+        {
+            get => _appTheme;
+            set
+            {
+                _appTheme = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string AppLanguage
+        {
+            get
+            {
+                return localSettings.Values[LanguageKey]?.ToString();
+            }
+            set
+            {
+                localSettings.Values[LanguageKey] = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void CheckTheme(FrameworkElement element)
+        {
+            if (element == null)
+                throw new ArgumentNullException(nameof(element));
+
+            if (element.RequestedTheme != AppTheme)
+            {
+                element.RequestedTheme = AppTheme;
+            }
+        }
+    }
+}
---
+++ UI/DI/DependencyConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogic.Interfaces;
+using BusinessLogic.Services.CoinCapApi;
+using BusinessLogic.Services.CoinGeckoApi;
+using Microsoft.Extensions.DependencyInjection;
+using UI.ViewModels;
+
+namespace UI.DI
+{
+    public static class DependencyConfiguration
+    {
+        public static IServiceCollection ConfigureDependency(this IServiceCollection services)
+        {
+            // Services
+            services.AddSingleton<ICurrencyService, CurrencyService>();
+            services.AddSingleton<IMarketService, MarketService>();
+            services.AddSingleton<IRatesService, RatesService>();
+            services.AddSingleton<ICandlesService, CandlesService>();
+
+            // ViewModels
+            services.AddSingleton<CandlesViewModel>();
+            services.AddSingleton<ConverterViewModel>();
+            services.AddSingleton<CurrencyViewModel>();
+            services.AddSingleton<MainViewModel>();
+
+            return services;
+        }
+    }
+}
